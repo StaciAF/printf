8e7894192776c98708a134b80bd1978a2d2cf8f3
StaciAF <aaen.it19@gmail.com>
2020-03-14 18:38:26 +0000

    updated print_func with return type and parameters - added variadic functions

--- print_funcs.c
+++ print_funcs.c
@@ -1,21 +1,40 @@
 #include "holberton.h"
 #include <unistd.h>
+#include <stdarg.h>
 /**
  * print_string - prints given string
- * @s: string passed to be printed
+ * @int: string passed to be printed
  *
  * Return: Nothing
  */
-void print_string(char *s)
+int print_string(const unsigned int n,...)
 {
+/* set variadic functions */
+	va_list args;
+
 /* set integer to count size of string */
 	int count;
+	char *s;
+
+/* initialize args list */
+	va_start (args, n);
+
+/* set s to arg list reading char pointer */
+	s = va_arg (args, char *);
 
 /* iterate through string to find length */
 	count = 0;
 	while (s[count] != '\0')
+	{
 		count++;
+	}
 
 /* prints string to stdout (1) */
 	write(1, s, count);
+
+/* end argument list */
+	va_end (args);
+
+/* returns number of characters printed */
+	return (count);
 }
