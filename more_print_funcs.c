2a742e49e119775b048555e0b03fbb6a893cff56
StaciAF <aaen.it19@gmail.com>
2020-03-17 22:15:51 +0000

    updated print_func line and function count

---
+++ more_print_funcs.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_bin - prints and unsigned int as binary
+ * @n: number of args
+ *
+ * Return: num of characters printed
+ */
+int print_bin(const unsigned int n, ...)
+{
+	unsigned int i, digit, num, count = 0;
+	char *s = malloc(sizeof(char) * 1);
+	char *p = s;
+	va_list args;
+
+	if (s == NULL)
+		return (-1);
+
+	/* Prepping variables for use */
+	va_start(args, n);
+	num = va_arg(args, unsigned int);
+	s[0] = '\0';
+
+	/* Algorithm to convert to binary string, digit by digit */
+	while (num != 0)
+	{
+		count++;
+		digit = num % 2;
+		num = num / 2;
+		p = s;
+		s = malloc(sizeof(char) * count);
+
+		if (s == NULL)
+			return (-1);
+
+		for (i = 0; i < count - 1; i++)
+			s[(count - 1) - i] = p[(count - 2) - i];
+		free(p);
+		s[0] = digit + '0';
+	}
+
+	write(1, s, count);
+
+	va_end(args);
+	free(s);
+
+	return (count);
+}
--- print_funcs.c
+++ print_funcs.c
@@ -108,8 +108,7 @@ int print_percent(const unsigned int n, ...)
  * Return: num of characters printed
  */
 int print_int(const unsigned int n, ...)
-{
-	int i, num, neg = 0, count = 0, div = 1;
+{       int i, num, neg = 0, count = 0, div = 1;
 	char *s;
 	va_list args;
 
@@ -129,10 +128,8 @@ int print_int(const unsigned int n, ...)
 		count++;
 	}
 	s = malloc(sizeof(char) * count);
-
 	if (s == NULL)
 		return (-1);
-
 	div = div / 10;
 	/* converts int into string */
 	for (i = 0; i < count; i++)
@@ -149,53 +146,6 @@ int print_int(const unsigned int n, ...)
 	write(1, s, count);
 	va_end(args);
 	free(s);
-	return (count);
-}
-
-/**
- * print_bin - prints and unsigned int as binary
- * @n: number of args
- *
- * Return: num of characters printed
- */
-int print_bin(const unsigned int n, ...)
-{
-	unsigned int i, digit, num, count = 0;
-	char *s = malloc(sizeof(char) * 1);
-	char *p = s;
-	va_list args;
-
-	if (s == NULL)
-		return (-1);
-
-	/* Prepping variables for use */
-	va_start(args, n);
-	num = va_arg(args, unsigned int);
-	s[0] = '\0';
-
-	/* Algorithm to convert to binary string, digit by digit */
-	while (num != 0)
-	{
-		count++;
-		digit = num % 2;
-		num = num / 2;
-		p = s;
-		s = malloc(sizeof(char) * count);
-
-		if (s == NULL)
-			return (-1);
-
-		for (i = 0; i < count - 1; i++)
-			s[(count - 1) - i] = p[(count - 2) - i];
-		free(p);
-		s[0] = digit + '0';
-	}
-
-	write(1, s, count);
-
-	va_end(args);
-	free(s);
-
 	return (count);
 }
 
